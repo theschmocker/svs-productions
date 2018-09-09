c3098017eb6851985061b8d18aed50ecb3fcc95d
theschmocker <jacobcs1996@gmail.com>
2018-09-09 12:59:53 -0500

    add Services section to page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Link from 'gatsby-link';
 
 import LandingHero, {
@@ -8,20 +8,27 @@ import LandingHero, {
 } from '../components/LandingHero';
 import Button from '../components/Button';
 import HeaderVideo from '../components/HeaderVideo';
+import Services from '../components/Services';
+
 import bg from '../../static/svs2.mp4';
 
 const IndexPage = () => (
-  <LandingHero>
-    <HeaderVideo>
-      <source src={bg} type="video/mp4" />
-    </HeaderVideo>
-    <Container>
-      <Heading>We do visual</Heading>
-      <SubHeading>Need quality visuals for your business or event? We have you covered.</SubHeading>
-      <Button primary>Hire us</Button>
-      <Button>See work</Button>
-    </Container>
-  </LandingHero>
+  <Fragment>
+    <LandingHero>
+      <HeaderVideo>
+        <source src={bg} type="video/mp4" />
+      </HeaderVideo>
+      <Container>
+        <Heading>We do visual</Heading>
+        <SubHeading>
+          Need quality visuals for your business or event? We have you covered.
+        </SubHeading>
+        <Button primary>Hire us</Button>
+        <Button>See work</Button>
+      </Container>
+    </LandingHero>
+    <Services />
+  </Fragment>
 );
 
 export default IndexPage;
