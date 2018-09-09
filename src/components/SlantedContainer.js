42766b39c8031f371296003640b34e02050ca4e9
theschmocker <jacobcs1996@gmail.com>
2018-09-09 13:04:40 -0500

    add SlantedContainer for Services

---
+++ src/components/SlantedContainer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const SlantedContainer = styled.section`
+  position: relative;
+  width: 100%;
+  margin-top: 100px;
+  background: ${props => props.theme.colors.fg};
+  z-index: 10000;
+  color: ${props => props.theme.colors.bg};
+  box-sizing: border-box;
+
+  &:before {
+    display: block;
+    position: absolute;
+    top: -100px;
+    content: '';
+    background-image: url("data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100' preserveAspectRatio='none'%3E%3Cpolygon points='0,100 100,0 100,100' style='fill:%23${props => props.theme.colors.fg.replace('#', '')}%3B' /%3E%3C/svg%3E");
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
+    height: 100px;
+    width: 100%;
+  }
+`;
+
+export default SlantedContainer;
