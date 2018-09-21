046b6015dee865d437864eee529dff346e529941
theschmocker <jacobcs1996@gmail.com>
2018-09-21 00:05:47 -0500

    remove react next plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,6 @@ module.exports = {
     title: 'SvS Productions',
   },
   plugins: [
-    'gatsby-plugin-react-next',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
   ],
